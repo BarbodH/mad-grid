84ff463ac871c6a8dab6358a8fda77b1a5248ca0
BarbodH <habibi.k.barbod@gmail.com>
2022-07-18 01:09:25 -0400

    Recreated a more visually aesthetic settings layout

--- app/src/main/java/com/example/mad_grid/SettingsActivity.java
+++ app/src/main/java/com/example/mad_grid/SettingsActivity.java
@@ -1,13 +1,12 @@
 package com.example.mad_grid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     // data variables
@@ -24,19 +23,11 @@ public class SettingsActivity extends AppCompatActivity {
         updateSettingsView();
     }
 
-    public void saveSettings(View view) {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        boolean musicOn = ((Switch) findViewById(R.id.settingsMusicSwitch)).isChecked();
-        boolean VibrationOn = ((Switch) findViewById(R.id.settingsVibrationSwitch)).isChecked();
-        boolean soundOn = ((Switch) findViewById(R.id.settingsSoundSwitch)).isChecked();
-        editor.putBoolean("Music", musicOn);
-        editor.putBoolean("Vibration", VibrationOn);
-        editor.putBoolean("Sound", soundOn);
-        Toast.makeText(this, "Settings Saved", Toast.LENGTH_SHORT).show();
-        editor.apply();
-    }
-
+    /**
+     * Loads previously saved settings from Shared Preferences
+     * Precondition(s): none
+     * Postcondition(s): settings boolean data variables are initialized with a default value of 'true'
+     */
     private void loadSettings() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         this.music = sharedPreferences.getBoolean("Music", true);
@@ -44,9 +35,37 @@ public class SettingsActivity extends AppCompatActivity {
         this.sound = sharedPreferences.getBoolean("Sound", true);
     }
 
+    /**
+     * Determine the state of switches based on previously saved settings in Shared Preferences
+     * Precondition(s): 'music', 'vibration', and 'sound' boolean data variables are already loaded & initialized
+     * Postcondition(s): Switches are checked/unchecked according to loaded boolean settings in Shared Preferences
+     */
     private void updateSettingsView() {
-        ((Switch) findViewById(R.id.settingsMusicSwitch)).setChecked(this.music);
-        ((Switch) findViewById(R.id.settingsVibrationSwitch)).setChecked(this.vibration);
-        ((Switch) findViewById(R.id.settingsSoundSwitch)).setChecked(this.sound);
+        ((SwitchCompat) findViewById(R.id.settingsMusicSwitch)).setChecked(this.music);
+        ((SwitchCompat) findViewById(R.id.settingsVibrationSwitch)).setChecked(this.vibration);
+        ((SwitchCompat) findViewById(R.id.settingsSoundSwitch)).setChecked(this.sound);
+    }
+
+    /**
+     * Saves settings and returns to homepage
+     * Precondition(s): none
+     * Postcondition(s): settings booleans are saved to Shared Preferences and MainActivity is started
+     * @param view - user interface
+     */
+    public void returnHome(View view) {
+        // save settings
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        boolean musicOn = ((SwitchCompat) findViewById(R.id.settingsMusicSwitch)).isChecked();
+        boolean VibrationOn = ((SwitchCompat) findViewById(R.id.settingsVibrationSwitch)).isChecked();
+        boolean soundOn = ((SwitchCompat) findViewById(R.id.settingsSoundSwitch)).isChecked();
+        editor.putBoolean("Music", musicOn);
+        editor.putBoolean("Vibration", VibrationOn);
+        editor.putBoolean("Sound", soundOn);
+        editor.apply();
+
+        // return to homepage (start MainActivity)
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
